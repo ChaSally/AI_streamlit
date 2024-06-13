6d936b1c98e8f32f64962263fa3d7f61a3953503
ChaSally <95266929+ChaSally@users.noreply.github.com>
2024-06-13 15:20:17 +0900

    Add files via upload

---
+++ 07_sidebar.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from PIL import Image
+
+# sidebar
+st.sidebar.title('Sidebar')
+st.sidebar.header('input text')
+user_id = st.sidebar.text_input('Inpu ID', value='streamlit', max_chars=15)
+user_pw = st.sidebar.text_input('Input PW', value='abcd', type='password')
+
+st.sidebar.header('select box')
+sel_opt = ['Vemeer', 'Munch', 'Shin']
+user_opt = st.sidebar.selectbox('favorite', sel_opt)
+st.sidebar.write(user_opt)
+
+# Main
+st.title('Title') ## write ()
+#folder = 'D:\AI_Chae\data\Vermeer.png'
+image_files = ['Vermeer.png', 'Munch.png', 'ShinYoonbok.png']
+
+sel_img_index = sel_opt.index(user_opt)
+# 선택한 항목에 맞는 이미지 파일 지정
+img_file = image_files[sel_img_index]
+img_local = Image.open(r'D:/AI_Chae/data/'+img_file) # data path /
+st.image(img_local, caption=user_opt) 
+
